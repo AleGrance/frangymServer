343b9fdece3f1724f9a2e153dc8b63ac3858a3e5
System Administrator <alegrance1000@gmail.com>
2022-10-20 14:37:54 -0300

    hola2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import consign from 'consign';
 
 const app = express();
 
-//cross access
+//cross access12345
 var allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
